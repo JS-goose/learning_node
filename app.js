a346e04216a69fe896b08663d3e0f88578ac07a3
Jonathan Sexton <jonathan.sexton@cloudinary.com>
2023-01-24 08:01:57 -0600

    notes added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // These lines are all that's required to create my own server - pretty cool!
+// Single threaded execution with an event loop (multi-threaded via os)
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  //   Hard quit of event loop - "powers down the server" - same as CTRL + C in terminal
+  //   process.exit()
 });
 
 server.listen(3000);
