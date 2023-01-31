248280b5d37c9042c3b3d64878b408041dfd3caa
Jonathan Sexton <jonathan.sexton@cloudinary.com>
2023-01-31 07:33:15 -0600

    not sure

--- app.js
+++ app.js
@@ -3,9 +3,25 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  // Routing
+  if (url === "/submit") {
+    res.write("<html>");
+    res.write("<head><title>Send Your Message to the Server</title></head>");
+    res.write("<body><input type='text'><button type='submit'>Submit</button></input></body></head>");
+    res.write("</html>");
+    res.end();
+  }
   //   Hard quit of event loop - "powers down the server" - same as CTRL + C in terminal
   //   process.exit()
+
+  //   There are packages that set this automatically for us
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Hello From Node</title></head>");
+  res.write("<body><h1>HELLO FROM NODE</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
