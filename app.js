94f186b04f830a7fadf199cb54b93aad1f285665
Jonathan Sexton <jonathan.sexton@cloudinary.com>
2023-01-31 07:41:42 -0600

    import fs & start redirect

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // These lines are all that's required to create my own server - pretty cool!
 // Single threaded execution with an event loop (multi-threaded via os)
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
@@ -8,13 +9,20 @@ const server = http.createServer((req, res) => {
   if (url === "/submit") {
     res.write("<html>");
     res.write("<head><title>Send Your Message to the Server</title></head>");
-    res.write("<body><input type='text'><button type='submit'>Submit</button></input></body></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='bacon text here'><button type='submit'>Submit</button></input></form></body></head>"
+    );
     res.write("</html>");
-    res.end();
+    // Return statement is not required to get a response but is used to end this function
+    return res.end();
   }
   //   Hard quit of event loop - "powers down the server" - same as CTRL + C in terminal
   //   process.exit()
 
+  if (url === "/message" && req.method === "POST") {
+    res.write();
+    return res.end();
+  }
   //   There are packages that set this automatically for us
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
