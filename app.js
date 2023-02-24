bf01130a968c139712ca5e1951bd482150b2dd1a
Jonathan Sexton <joncsexton@gmail.com>
2023-02-24 11:36:43 -0600

    #3 on assignments

--- app.js
+++ app.js
@@ -11,19 +11,24 @@ const express = require('express');
 const app = express();
 
 // * If there's middleware that should execute for all requests, it needs to be first in line //
+// * By default, express will set the header type but I can override if necessary
 
-app.use('/', (req, res, next) => {
-  console.log('Here is the first middleware call');
-  // By default, express will set the header type but I can override if necessary
-  next();
+// app.use('/', (req, res, next) => {
+//   console.log('Here is the first middleware call');
+//   next();
+// });
+
+// app.use('/', (req, res, next) => {
+//   console.log('Here is the second middleware call');
+//   res.send('<h1>Response sent from second middleware!</h1>');
+// });
+
+app.use('/users', (req, res) => {
+  res.send('<h1>This is the response for "/users"</h1><button>Home</button>');
 });
-
-app.use('/', (req, res, next) => {
-  console.log('Here is the second middleware call');
-  // By default, express will set the header type but I can override if necessary
-  res.send('<h1>Response sent from second middleware!</h1>');
+app.use((req, res) => {
+  res.send('<h1>Response for "/"</h1>');
 });
-
 app.listen(3000);
 // * app.listen does the same thing as these 2 lines of Node
 // createServer() has a built in event listener - with the example below listening for all req/res //
