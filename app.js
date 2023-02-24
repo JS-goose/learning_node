c26897840a9e44e07b03e5a3758a3c17f5f3a576
Jonathan Sexton <joncsexton@gmail.com>
2023-02-24 10:54:17 -0600

    #2 on assignments

--- app.js
+++ app.js
@@ -12,13 +12,19 @@ const app = express();
 
 // * If there's middleware that should execute for all requests, it needs to be first in line //
 
-app.use('/bacon',(req, res, next) => {
-    console.log('Another middleware')
-    // By default, express will set the header type but I can override if necessary
-    res.send('<h1>Response sent!</h1>')
-})
+app.use('/', (req, res, next) => {
+  console.log('Here is the first middleware call');
+  // By default, express will set the header type but I can override if necessary
+  next();
+});
 
-app.listen(3000)
+app.use('/', (req, res, next) => {
+  console.log('Here is the second middleware call');
+  // By default, express will set the header type but I can override if necessary
+  res.send('<h1>Response sent from second middleware!</h1>');
+});
+
+app.listen(3000);
 // * app.listen does the same thing as these 2 lines of Node
 // createServer() has a built in event listener - with the example below listening for all req/res //
 // This event listener is never done so this function will always be in the event loop //
