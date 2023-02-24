bd97ae41cd763464b6a080e1d2defe392cfad76e
Jonathan Sexton <jonathan.sexton@cloudinary.com>
2023-02-24 10:39:15 -0600

    reworked server listen & added express

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-// These lines are all that's required to create my own server - pretty cool!
+//  These lines are all that's required to create my own server - pretty cool!
 // Single threaded execution with an event loop (multi-threaded via os)
-const http = require("http");
+// ! Not required as Express handles this for me
+// const http = require('http');
 // Node caches the file contet of the import thus making this a READ Only import //
 // routes.newFunc = (data) => {console.log(data)} would not manipulate the original //
 // file although it's possible to have functions within the routes file that could accomplish this //
-const routes = require("./routes");
+// const routes = require("./routes");
 
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('This is the next middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+    console.log('Another middleware')
+    // By default, express will set the header type but I can override if necessary
+    res.send('<h1>Response sent!</h1>')
+})
+
+app.listen(3000)
+// * app.listen does the same thing as these 2 lines of Node
 // createServer() has a built in event listener - with the example below listening for all req/res //
 // This event listener is never done so this function will always be in the event loop //
-const server = http.createServer();
-
-server.listen(3000);
+// const server = http.createServer(app);
+// server.listen(3000);
