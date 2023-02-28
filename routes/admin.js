784e027c86c5923b6cc1bb26aab4bb3bead5565b
Jonathan Sexton <joncsexton@gmail.com>
2023-02-28 09:18:13 -0600

    sending static files

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+// * Path is a core Node module and constructs paths that work on all operating systems
+const path = require('path');
 
 router.get('/users', (req, res) => {
   res.send(
     '<h1>Add User</h1><form action="/adduser" method="POST"><input type="text" name="username"><button type="submit">Submit</button></form>'
   );
+});
+
+router.use('/add-product', (req, res) => {
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post('/adduser', (req, res) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
-router.get((req, res) => {
-  res.send('<h1>Response for "/"</h1>');
+// router.get('/', (req, res) => {
+//   res.send('<h1>Response for "/"</h1>');
+// });
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
